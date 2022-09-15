41c567edd95661db14cd29d28b872432094cb92d
Samuel <samuelsantos@sescmg.com.br>
2022-09-15 15:40:55 -0300

    Alterações Database

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -33,9 +33,9 @@ class Database extends Config
     public $default = [
         'DSN'      => '',
         'hostname' => 'localhost',
-        'username' => '',
-        'password' => '',
-        'database' => '',
+        'username' => 'samuel',
+        'password' => 'Sesc2002*',
+        'database' => 'samueldatabase',
         'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
         'pConnect' => false,
